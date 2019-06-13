7746b5dcd07bcd9f664b9c1facd583b885f3a668
Keith <rickyprev1928@gmail.com>
2019-06-12 22:20:51 -0400

    section2 portfolio and text. Needs dynamic. 2.0

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -1,19 +1,71 @@
   
-import React, { Component, Fragment } from 'react';
+import React, { Component} from 'react';
 import './ProjectCard.css';
 
 
-const ProjectCard= props => (
-	<div className="projectCardWrapper">
-		<div className="projectCardImage" >
-			<img classname="theImage" src={props.imageUrl}  alt="" />
-			
-		</div>
+class ProjectCard extends Component{
+
+	constructor(props) {
+		super(props);
+		this.state = {
+		
+	}
+	
+};
+
 
 	
 
-	</div>
-);
+	render(){
+		const projects = [
+			{
+				title: "Shuttler App",
+				source: require("../../../Assets/shuttlerLogo.png"),
+				color: "#E50914",
+				link: "https://www.psushuttle.com/",
+				description:
+					"This application is developed to help hundreds of students on campus to track the location of the campus shuttle. It helps save student time by notifying them exactly when the shuttle reaches the location they specified in their preferences."
+			},
+			{
+				title: "DeliverIt App",
+				source: require("../../../Assets/deliverItLogo.png"),
+				color: "#E50914",
+				link: "https://github.com/PSUCoders/DeliverIt",
+				description:
+					"A campus solution to help deliver groceries and other college student needs as a mobile app."
+			},
+			{
+				title: "FindIt App",
+				source: require("../../../Assets/findItLogo.png"),
+				color: "#E50914",
+				link: "http://finditapp.biz/",
+				description:
+					"This application is developed to help hundreds of students on campus to track the location of the campus shuttle. It helps save student time by notifying them exactly when the shuttle reaches the location they specified in their preferences."
+			}
+			
+		];
+		return(
+			<div className="projectCardWrapper">
+			<div className="projectCardImage" >
+				{
+					projects.map(projects=>
+						<img classname={projects.title} src={projects.source}  alt="empty" />
+
+					)
+				}
+				
+			</div>
+	
+		
+	
+		</div>
+
+		);
+	}
+	
+}
+
+
 
 
 export default ProjectCard;
--- src/Components/Sections/Section2/Section2.js
+++ src/Components/Sections/Section2/Section2.js
@@ -10,30 +10,7 @@ class Section2 extends Component {
     constructor(props) {
 		super(props);
 
-		this.projects = [
-			{
-				title: "Shuttler App",
-				color: "#E50914",
-				link: "https://www.psushuttle.com/",
-				description:
-					"This application is developed to help hundreds of students on campus to track the location of the campus shuttle. It helps save student time by notifying them exactly when the shuttle reaches the location they specified in their preferences."
-            },
-            {
-				title: "DeliverIt App",
-				color: "#E50914",
-				link: "https://github.com/PSUCoders/DeliverIt",
-				description:
-					"A campus solution to help deliver groceries and other college student needs as a mobile app."
-            },
-            {
-				title: "FindIt App",
-				color: "#E50914",
-				link: "http://finditapp.biz/",
-				description:
-					"This application is developed to help hundreds of students on campus to track the location of the campus shuttle. It helps save student time by notifying them exactly when the shuttle reaches the location they specified in their preferences."
-            }
-            
-		];
+
 	}
 
     render() {
@@ -45,10 +22,7 @@ class Section2 extends Component {
                     <div className="whiteBox">
                     <ProjectCard className="Shuttler" projectName="Shuttler" imageUrl={Shuttler} description= "Web App" >
                     </ProjectCard>
-                    <ProjectCard className="DeliverIt" projectName="DeliverIt" imageUrl={DeliverIt} description= "Web App">
-                    </ProjectCard>
-                    <ProjectCard className="FindIt" projectName="Shuttler" imageUrl={FindIt} description= "Web App">
-                    </ProjectCard>
+               
                     </div>
                     </div>
                     <div className="sectionTwoText">
