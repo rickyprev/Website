98baad5f80f0de7990da58ee8f933de3c27db179
Keith <rickyprev1928@gmail.com>
2019-06-13 11:58:02 -0400

    .map works for array but logos arent loading.

--- src/Components/ProjectCard/ProjectCard.js
+++ src/Components/ProjectCard/ProjectCard.js
@@ -1,6 +1,8 @@
   
 import React, { Component} from 'react';
 import './ProjectCard.css';
+import Shuttler from "../../../Assets/shuttlerLogo.png";
+import DeliverIt from "../../../Assets/deliverItLogo.png";
 
 
 class ProjectCard extends Component{
@@ -8,6 +10,8 @@ class ProjectCard extends Component{
 	constructor(props) {
 		super(props);
 		this.state = {
+			
+
 		
 	}
 	
@@ -17,43 +21,16 @@ class ProjectCard extends Component{
 	
 
 	render(){
-		const projects = [
-			{
-				title: "Shuttler App",
-				source: require("../../../Assets/shuttlerLogo.png"),
-				color: "#E50914",
-				link: "https://www.psushuttle.com/",
-				description:
-					"This application is developed to help hundreds of students on campus to track the location of the campus shuttle. It helps save student time by notifying them exactly when the shuttle reaches the location they specified in their preferences."
-			},
-			{
-				title: "DeliverIt App",
-				source: require("../../../Assets/deliverItLogo.png"),
-				color: "#E50914",
-				link: "https://github.com/PSUCoders/DeliverIt",
-				description:
-					"A campus solution to help deliver groceries and other college student needs as a mobile app."
-			},
-			{
-				title: "FindIt App",
-				source: require("../../../Assets/findItLogo.png"),
-				color: "#E50914",
-				link: "http://finditapp.biz/",
-				description:
-					"This application is developed to help hundreds of students on campus to track the location of the campus shuttle. It helps save student time by notifying them exactly when the shuttle reaches the location they specified in their preferences."
-			}
-			
-		];
+		
 		return(
 			<div className="projectCardWrapper">
 			<div className="projectCardImage" >
 				{
-					projects.map(projects=>
-						<img classname={projects.title} src={projects.source}  alt="empty" />
+				<img classname={this.props.url} src={this.props.logo}  alt="empty" />
 
-					)
-				}
 				
+				}
+				{/* <p>{this.props.url}</p> */}
 			</div>
 	
 		
--- src/Components/Sections/Section2/Section2.js
+++ src/Components/Sections/Section2/Section2.js
@@ -4,7 +4,7 @@ import './Section2.css';
 import ProjectCard from "../../ProjectCard/ProjectCard.js";
 import Shuttler from "../../../../Assets/shuttlerLogo.png";
 import DeliverIt from "../../../../Assets/deliverItLogo.png";
-import FindIt from "../../../../Assets/findItLogo.png";
+// import FindIt from "../../../../Assets/findItLogo.png";
 
 class Section2 extends Component {
     constructor(props) {
@@ -14,14 +14,48 @@ class Section2 extends Component {
 	}
 
     render() {
-
+        let projects = [
+			{
+				title: "Shuttler App",
+				logo: {Shuttler},
+				color: "#E50914",
+				link: "https://www.psushuttle.com/",
+				description:
+					"This application is developed to help hundreds of students on campus to track the location of the campus shuttle. It helps save student time by notifying them exactly when the shuttle reaches the location they specified in their preferences."
+			},
+			{
+				title: "DeliverIt App",
+				logo: {DeliverIt},
+				color: "#E50914",
+				link: "https://github.com/PSUCoders/DeliverIt",
+				description:
+					"A campus solution to help deliver groceries and other college student needs as a mobile app."
+			},
+			// {
+			// 	title: "FindIt App",
+			// 	logo: require("../../../Assets/findItLogo.png"),
+			// 	color: "#E50914",
+			// 	link: "http://finditapp.biz/",
+			// 	description:
+			// 		"This application is developed to help hundreds of students on campus to track the location of the campus shuttle. It helps save student time by notifying them exactly when the shuttle reaches the location they specified in their preferences."
+			// }
+			
+		];
         return (
+                
                 <div className="sectionTwo">
                 <div className="sectionTwoBackground">
                 <h1 className="sectionTwoTitle">Portfolio</h1>
                     <div className="whiteBox">
-                    <ProjectCard className="Shuttler" projectName="Shuttler" imageUrl={Shuttler} description= "Web App" >
-                    </ProjectCard>
+                        
+                            {/* <ProjectCard classname={projects.title} src={projects.source}  imageUrl={projects.source} description= "Web App" alt="empty" /> */}
+
+                        {projects.map( (item,index)=>(
+
+							<ProjectCard key={item.link} title={item.title} logo={item.logo} url={item.link} description={item.description}/>
+						))}
+
+					
                
                     </div>
                     </div>
@@ -32,6 +66,8 @@ class Section2 extends Component {
 
                     </div>
                 </div>
+                
+               
         );
     }
 }
